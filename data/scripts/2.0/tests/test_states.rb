71cfaffbacbfb74d64e88d114bf546c8464bc87e
Hanmac <hanmac@gmx.de>
2013-04-12 08:40:40 +0200

    add TestCase for the States and test the auto_state functions

---
+++ data/scripts/2.0/tests/test_states.rb
@@ -0,0 +1,85 @@
+require "test/unit"
+require "turn/autorun"
+
+require_relative "../enemy_state"
+
+class StateTest < Test::Unit::TestCase
+ 
+  def setup
+    s=RPG::State.new(:burn)
+    s.states_chance[:freeze] = 0.5
+   
+    s=RPG::State.new(:freeze)
+    
+    #st=RPG::Weapon.new(:staff)
+    #st.states_chance[:freeze] = 0.5
+    #warrior=RPG::ActorClass.new(:warrior)
+    #warrior.states_chance[:freeze] = 0.5
+    
+    e = RPG::Enemy.new(:slime)
+    e.auto_states << :burn
+    #e.states_chance[:freeze] = 0.5
+    
+    e = RPG::Enemy.new(:wizzard)
+    e.states_chance[:freeze] = 0.5
+    
+    e = RPG::Enemy.new(:human)
+          
+  end
+  
+  def test_enemy_state_chance
+    gs=Game::Enemy.new(:slime)
+    
+    assert_equal({:freeze => 0.5},gs.states_chance)
+    assert_equal(0.5,gs.states_chance(:freeze))
+    assert_equal(1.0,gs.states_chance(:fire))
+    
+    gw=Game::Enemy.new(:wizzard)
+    
+    assert_equal({:freeze => 0.5},gw.states_chance)
+    assert_equal(0.5,gw.states_chance(:freeze))
+    assert_equal(1.0,gw.states_chance(:fire))
+      
+    gh = Game::Enemy.new(:human)
+
+    assert_equal({},gh.states_chance)
+    assert_equal(1.0,gh.states_chance(:freeze))
+    assert_equal(1.0,gh.states_chance(:fire))
+
+    gh.add_state(:burn)
+
+    assert_equal({:freeze => 0.5},gh.states_chance)
+    assert_equal(0.5,gh.states_chance(:freeze))
+    assert_equal(1.0,gh.states_chance(:fire))
+
+  end
+  
+  def test_enemy_auto_states
+    gs=Game::Enemy.new(:slime)
+     
+    assert_not_empty(gs.states)
+    assert_not_empty(gs.states(:burn))
+    assert_empty(gs.states(:freeze))
+    
+    gw=Game::Enemy.new(:wizzard)
+    
+    assert_empty(gw.states)
+    assert_empty(gw.states(:burn))
+    assert_empty(gw.states(:freeze))
+  
+    gh=Game::Enemy.new(:human)
+      
+    assert_empty(gh.states)
+    assert_empty(gh.states(:burn))
+    assert_empty(gh.states(:freeze))
+
+    gh.add_state(:burn)
+
+    assert_not_empty(gh.states)
+    assert_not_empty(gh.states(:burn))
+    assert_empty(gh.states(:freeze))
+        
+  end
+  
+  
+end
