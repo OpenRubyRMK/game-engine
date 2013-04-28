b401d5151abbe57c115f5b928c84c53eb507b9c8
Hanmac <hanmac@gmx.de>
2013-04-28 21:26:02 +0200

    Add Skills to Mastery that will available when the mastery levels up and the right equip_type is equipped

---
+++ data/scripts/2.0/mastery_skill.rb
@@ -0,0 +1,26 @@
+require_relative "battler_mastery"
+require_relative "battler_skill"
+require_relative "levelable_skill"
+
+module Game
+  class Mastery
+    def skills(k=nil)
+      list = (0).upto(level).map {|l|
+        l = @levels[l]
+        l.nil? ? [] : k ? l.skills[k] : l.skills.values
+      }.compact.flatten
+
+      return k ? list : list.group_by(&:name)
+    end
+  end
+
+  class Battler
+
+    chain "MasterySkillInfluence" do
+			def _skills(key)
+			        super + active_mastery.each_value.map{|m| key ? m.skills(key) : m.skills.values }.flatten
+			      end
+    end
+
+  end
+end
